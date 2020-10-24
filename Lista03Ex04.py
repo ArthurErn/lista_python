ded72206423247cf40f793f98ba5ce547ba8f9e0
ArthurErn <arthurern48@gmail.com>
2020-10-24 13:11:52 -0300

    lista de exercicios python

---
+++ Lista03Ex04.py
@@ -0,0 +1,53 @@
+print("\nTabuada do 1\n")
+
+for i in range(0, 10):
+    num = 1 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 2\n")
+
+for i in range(0, 10):
+    num = 2 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 3\n")
+
+for i in range(0, 10):
+    num = 3 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 4\n")
+
+for i in range(0, 10):
+    num = 4 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 5\n")
+
+for i in range(0, 10):
+    num = 5 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 6\n")
+
+for i in range(0, 10):
+    num = 6 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 7\n")
+
+for i in range(0, 10):
+    num = 7 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 8\n")
+
+for i in range(0, 10):
+    num = 8 * i
+    print(f'{num}\n')
+
+print("\nTabuada do 9\n")
+
+for i in range(0, 10):
+    num = 9 * i
+    print(f'{num}\n')
